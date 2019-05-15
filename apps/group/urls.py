1ae9eb3b303ef31e221a3fddf78c9d8da19de64d
ThreeH <995199838@qq.com>
2019-05-15 16:56:49 +0800

    del gruop

--- apps/group/urls.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from apps.group import view
-
-group = Blueprint('group', __name__)
-
-@group.route('/')
-def root():
-    return view.root()
--- apps/group/view.py
+++
@@ -1,4 +0,0 @@
-
-
-def root():
-    return 'This is root of group'
