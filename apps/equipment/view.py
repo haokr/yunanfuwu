bfcffea5f8580d4818642ca3a87e7aed3bd64f3e
k123k <41769593+emptyall@users.noreply.github.com>
2019-05-15 18:10:35 +0800

    equipment note
    equipment note

--- apps/equipment/view.py
+++ apps/equipment/view.py
@@ -3,13 +3,20 @@ from models import User, Group, Equipment
 from db import db
 
 
-# 设备获取
 def getEquipments():
+    '''
+        设备信息获取
+    :return:
+    '''
     return redirect('/monitor')
 
 
-# 新增设备方法
 def addEquipment():
+    '''
+        新增设备
+        将新设备添加至数据库
+    :return: 数据添加结果
+    '''
     name = request.form.get('name')
     user_id = session.get('id')
     if not user_id:
@@ -29,8 +36,13 @@ def addEquipment():
     return jsonify({'msg': 'success', 'data': 'add success'})
 
 
-# 修改设备信息方法
 def modifyEquipment(eid):
+    '''
+        获取用户提交要修改的设备信息
+        修改设备信息并存储
+    :param eid: 用户ID
+    :return: 设备信息修改状态
+    '''
     key = request.form.get('key')
     value = request.form.get('value')
     if value == '':
@@ -46,8 +58,13 @@ def modifyEquipment(eid):
         return jsonify({'msg': 'fail', 'data': 'modify equipment error when select equipments'})
 
 
-# 设备信息展示方法
 def showEquipments():
+    '''
+        查询当前用户拥有的设备
+        获取设备信息
+        设备信息展示
+    :return: 设备信息
+    '''
     user_id = session.get('id')
     equipments = User.query.filter(User.id == user_id).first().group.equipments
     data = {
@@ -75,8 +92,13 @@ def showEquipments():
     return render_template('equipments.html', **data)
 
 
-# 设备信息修改方法
 def showEditEquipment(eid):
+    '''
+        通过设备ID查询设备信息
+        设备信息修改并存储
+    :param eid: 设备ID
+    :return:
+    '''
     e = Equipment.query.filter(Equipment.id == eid).first()
     data = {
         'base':{
@@ -103,8 +125,11 @@ def showEditEquipment(eid):
     return render_template('editEquipment.html', **data)
 
 
-# 新增设备展示方法
 def showAddEquipment():
+    '''
+        新增设备展示
+    :return:
+    '''
     data = {
         'base':{
             'pageTitle': '添加设备-云安服务',
